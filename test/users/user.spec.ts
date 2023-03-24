faeb82575d20c39fb512480fe8ab6dadebeb6bb4
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-03-24 12:54:13 -0300

    :bricks: db: user migration created and configured

--- test/users/user.spec.ts
+++ test/users/user.spec.ts
@@ -3,9 +3,33 @@ import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
+/*
+  {
+    "users": {
+    "id": number,
+      "email": string,
+      "username": string,
+      "password": string,
+      "avatar": string
+    }
+  }
+*/
+
 test.group('User', () => {
   test.only('it should be able to create a new user', async (assert) => {
-    const userPayload = { email: 'test@test.com', username: 'test', password: 'test' }
-    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    const userPayload = {
+      email: 'test@test.com',
+      username: 'test',
+      password: 'test',
+      avatar: 'https://images.com/image/1',
+    }
+    // await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.user.id, 'Id undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.password, userPayload.password)
+    assert.equal(body.user.avatar, userPayload.avatar)
   })
 })
