18630c68e0c018a196fad17f2bec1164cd2d66ff
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-03-24 16:32:14 -0300

    :construction: feat: BadRequestException added

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,9 +1,20 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequest from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
 
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayload = request.only(['email', 'username', 'password', 'avatar'])
+
+    if (!userPayload.email || !userPayload.username || !userPayload.password)
+      throw new BadRequest('provide required data', 422)
+
+    const userByEmail = await User.findBy('email', userPayload.email)
+    const userByUsername = await User.findBy('username', userPayload.username)
+
+    if (userByEmail) throw new BadRequest('email already in use', 409)
+    if (userByUsername) throw new BadRequest('username already in use', 409)
+
     const user = await User.create(userPayload)
     return response.created({ user })
   }
--- test/users/user.spec.ts
+++ test/users/user.spec.ts
@@ -1,29 +1,18 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
-/*
-  {
-    "users": {
-    "id": number,
-      "email": string,
-      "username": string,
-      "password": string,
-      "avatar": string
-    }
-  }
-*/
-
-test.group('User', () => {
-  test.only('it should be able to create a new user', async (assert) => {
+test.group('User', (group) => {
+  test('it should be able to create a new user', async (assert) => {
     const userPayload = {
       email: 'test@test.com',
       username: 'test',
       password: 'test',
       avatar: 'https://images.com/image/1',
     }
-    // await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
     const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
     assert.exists(body.user, 'User undefined')
     assert.exists(body.user.id, 'Id undefined')
@@ -31,5 +20,56 @@ test.group('User', () => {
     assert.equal(body.user.username, userPayload.username)
     assert.notExists(body.user.password, 'Password defined')
     assert.equal(body.user.avatar, userPayload.avatar)
+  })
+
+  test('it should return 409 when email is already in use', async (assert) => {
+    const { email } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL)
+      .post('/users')
+      .send({
+        email,
+        username: 'test',
+        password: 'test',
+      })
+      .expect(409)
+
+    assert.exists(body.message)
+    assert.exists(body.code)
+    assert.exists(body.status)
+    assert.include(body.message, 'email')
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 409)
+  })
+
+  test('it should return 409 when username is already in use', async (assert) => {
+    const { username } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL)
+      .post('/users')
+      .send({
+        username,
+        email: 'test@test.com',
+        password: 'test',
+      })
+      .expect(409)
+
+    assert.exists(body.message)
+    assert.exists(body.code)
+    assert.exists(body.status)
+    assert.include(body.message, 'username')
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 409)
+  })
+
+  test.only('it should return 422 when required data is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/users').send({}).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
   })
 })
