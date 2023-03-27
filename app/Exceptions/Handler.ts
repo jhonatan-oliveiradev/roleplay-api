ac4d3c362a142313fc7f0de86fc32bc2c03289eb
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-03-27 08:54:19 -0300

    :test_tube: test: tests for email or password wrong implemented

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -1,23 +1,21 @@
-/*
-|--------------------------------------------------------------------------
-| Http Exception Handler
-|--------------------------------------------------------------------------
-|
-| AdonisJs will forward all exceptions occurred during an HTTP request to
-| the following class. You can learn more about exception handling by
-| reading docs.
-|
-| The exception handler extends a base `HttpExceptionHandler` which is not
-| mandatory, however it can do lot of heavy lifting to handle the errors
-| properly.
-|
-*/
-
-import Logger from '@ioc:Adonis/Core/Logger'
+import { Exception } from '@adonisjs/core/build/standalone'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import Logger from '@ioc:Adonis/Core/Logger'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error: Exception, ctx: HttpContextContract) {
+    if (error.status === 422)
+      return ctx.response.status(error.status).send({
+        code: 'BAD_REQUEST',
+        message: error.message,
+        status: error.status,
+        errors: error['messages']?.errors ? error['messages'].errors : '',
+      })
+    return super.handle(error, ctx)
   }
 }
--- test/users/user.spec.ts
+++ test/users/user.spec.ts
@@ -19,7 +19,6 @@ test.group('User', (group) => {
     assert.equal(body.user.email, userPayload.email)
     assert.equal(body.user.username, userPayload.username)
     assert.notExists(body.user.password, 'Password defined')
-    assert.equal(body.user.avatar, userPayload.avatar)
   })
 
   test('it should return 409 when email is already in use', async (assert) => {
@@ -60,8 +59,34 @@ test.group('User', (group) => {
     assert.equal(body.status, 409)
   })
 
-  test.only('it should return 422 when required data is not provided', async (assert) => {
+  test('it should return 422 when required data is not provided', async (assert) => {
     const { body } = await supertest(BASE_URL).post('/users').send({}).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
+  test('it should return 422 when providing an invalid email', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/users')
+      .send({
+        email: 'test@',
+        password: 'test',
+        username: 'test',
+      })
+      .expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
+  test('it should return 422 when providing an invalid password', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/users')
+      .send({
+        email: 'test@test.com',
+        password: 'tes',
+        username: 'test',
+      })
+      .expect(422)
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 422)
   })
