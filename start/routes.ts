d9a096653ee0892ee72a649f203613fe8816f1a9
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-04-05 08:37:13 -0300

    :construction: recovery password body email created

--- start/routes.ts
+++ start/routes.ts
@@ -6,3 +6,4 @@ Route.get('/', async () => {
 
 Route.post('/users', 'UsersController.store')
 Route.put('/users/:id', 'UsersController.update')
+Route.post('/forgot-password', 'PasswordController.forgotPassword')
---
+++ test/users/passwords.spec.ts
@@ -0,0 +1,42 @@
+import Mail from '@ioc:Adonis/Addons/Mail'
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Password', (group) => {
+  test('it should send an email with forgot password instructions', async (assert) => {
+    const user = await UserFactory.create()
+
+    Mail.trap((message) => {
+      assert.deepEqual(message.to, [
+        {
+          address: user.email,
+        },
+      ])
+      assert.deepEqual(message.from, {
+        address: 'no-reply@roleplay.com',
+      })
+      assert.equal(message.subject, 'Roleplay: Recuperação de Senha')
+      assert.include(message.html!, user.username)
+    })
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPasswordUrl: 'url',
+      })
+      .expect(204)
+
+    Mail.restore()
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+})
