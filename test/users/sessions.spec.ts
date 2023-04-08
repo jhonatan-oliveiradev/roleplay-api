0f7743a5d5a8cad4fc6e1bd7ac81861ef8145935
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-04-08 14:12:04 -0300

    :construction: feat: auth method created

---
+++ test/users/sessions.spec.ts
@@ -0,0 +1,27 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Session', (group) => {
+  test('it should authenticate an user', async (assert) => {
+    const plainPassword = 'test'
+    const { id, email } = await UserFactory.merge({ password: plainPassword }).create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email, password: plainPassword })
+      .expect(201)
+
+    assert.isDefined(body.user, 'User undefined')
+    assert.equal(body.user.id, id)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+})
